24f8ddcdcf470e01ebbb6566fb3dfb198c01568f
Zachary Eggert <Eggert.Zachary@gmail.com>
2025-02-06 17:16:53 -0800

    version bumps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,24 @@
 import { HydrateClient } from "~/trpc/server";
+import Link from "next/link";
 
 export default async function Home() {
   return (
     <HydrateClient>
-      <main className=""></main>
+      <main className="px-4 py-8">
+        <h2 className="text-xl">Categories</h2>
+        <ul>
+          <li className="">
+            <Link href="/electric-guitar" className="underline">
+              Electric Guitar
+            </Link>
+          </li>
+          <li className="">
+            <Link href="/amplifier" className="underline">
+              Amplifier
+            </Link>
+          </li>
+        </ul>
+      </main>
     </HydrateClient>
   );
 }
--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,31 +1,13 @@
-import { z } from "zod";
-
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+
+import { z } from "zod";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
-    .query(({ input }) => {
+    .query(async ({ input, ctx }) => {
       return {
-        greeting: `Hello ${input.text}`,
+        greeting: `Hello ${input.text} ${(await ctx.db.nut.findFirst())?.name ?? ""}`,
       };
     }),
-
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
-      return ctx.db.post.create({
-        data: {
-          name: input.name,
-        },
-      });
-    }),
-
-  getLatest: publicProcedure.query(async ({ ctx }) => {
-    const post = await ctx.db.post.findFirst({
-      orderBy: { createdAt: "desc" },
-    });
-
-    return post ?? null;
-  }),
 });
