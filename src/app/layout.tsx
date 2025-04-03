763071f9165b65fe36c2285f07e4342f244a5479
Zachary Eggert <Eggert.Zachary@gmail.com>
2025-04-03 13:08:36 -0700

    --

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,32 @@
 import "~/styles/globals.css";
 
-import { GeistSans } from "geist/font/sans";
-import { type Metadata } from "next";
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
 import { Provider as JotaiProvider } from "jotai";
-
 import { TRPCReactProvider } from "~/trpc/react";
 
 export const metadata: Metadata = {
-  title: "Diablo Description Generator v3",
-  description:
-    "An AI-less description generator for musical instrument classifieds",
-  icons: [{ rel: "icon", url: "/favicon.ico" }],
+	title: "Diablo Description Generator v3",
+	description:
+		"An AI-less description generator for musical instrument classifieds",
+	icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const geist = Geist({
+	subsets: ["latin"],
+	variable: "--font-geist-sans",
+});
+
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{ children: React.ReactNode }>) {
-  return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body className="bg-neutral-300 text-neutral-900 dark:bg-neutral-800 dark:text-neutral-100">
-        <JotaiProvider>
-          <TRPCReactProvider>{children}</TRPCReactProvider>
-        </JotaiProvider>
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${geist.variable}`}>
+			<body>
+				<JotaiProvider>
+					<TRPCReactProvider>{children}</TRPCReactProvider>
+				</JotaiProvider>
+			</body>
+		</html>
+	);
 }
